b8766bf5199df59dc3b0dac98dbc17d136c8e277
SETISAEDU <setisa2016@gmail.com>
2017-01-20 15:44:55 -0600

    Add files via upload

---
+++ Códigos de Arduino/RF433MHz_Receiver/RF433MHz_Receiver.ino
@@ -0,0 +1,120 @@
+// Librerías:
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+#include <VirtualWire.h>
+
+// Instanciacion del LCD i2c:
+LiquidCrystal_I2C lcd(0x27, 20, 4);
+
+// definicion de nuevos caracter
+#define CIRCLE 0
+#define BLANK 1
+#define TEMP 2
+#define HUM 3
+
+byte customChar[8] = {
+  B00100,
+  B01010,
+  B00100,
+  B00000,
+  B00000,
+  B00000,
+  B00000,
+  B00000
+};
+byte customChar1[8] = {
+  B00000,
+  B00000,
+  B00000,
+  B00000,
+  B00000,
+  B00000,
+  B00000,
+  B00000
+};
+
+byte customChar2[8] = {
+  B00100,
+  B01110,
+  B01110,
+  B01110,
+  B01110,
+  B11101,
+  B11101,
+  B01110
+};
+
+byte customChar3[8] = {
+  B00100,
+  B00100,
+  B01110,
+  B01010,
+  B10001,
+  B10001,
+  B01110,
+  B00000
+};
+
+//Variables de los datos recibidos
+float dato1 = 0.0;
+float dato2 = 0.0;
+
+
+void setup()
+{
+  //Inicializacion del LCD
+  lcd.begin();
+  lcd.backlight();
+  lcd.setCursor(0, 0);  // Se pone el cursor en la primera posición de la pantalla.
+  lcd.print("Iniciando...");
+  delay(1000);
+  lcd.createChar(CIRCLE, customChar);
+  lcd.createChar(BLANK, customChar1);
+  lcd.createChar(TEMP, customChar2);
+  lcd.createChar(HUM, customChar3);
+  lcd.clear();
+
+  // Se inicializa el RF
+  vw_setup(2000);  // velocidad: Bits per segundo
+  vw_rx_start();   // Se inicia como receptor
+}
+
+void loop()
+{
+  uint8_t buf[VW_MAX_MESSAGE_LEN];
+  uint8_t buflen = VW_MAX_MESSAGE_LEN;
+
+  //verificamos si hay un dato valido en el RF
+  if (vw_get_message((uint8_t *)buf, &buflen))
+  {
+    int i;
+    String  DatoCadena = "";
+    if ((char)buf[0] == 'i') //verificamos el inicio de trama
+    {
+      for (i = 1; i < buflen; i++)
+      {
+        DatoCadena.concat((char)buf[i]);
+      }
+      dato1 = DatoCadena.toFloat();
+      Serial.print("Humedad recibido: ");
+      Serial.println(dato1);
+      lcd.setCursor(0, 0);  // Se pone el cursor en la primera posición de la pantalla.
+      lcd.print("Hum: "); lcd.print(dato1); lcd.print(" %");
+      lcd.setCursor(15, 0); lcd.write(HUM);
+    }
+    else if ((char)buf[0] == 'f') //verificamos el inicio de trama
+    {
+      for (i = 1; i < buflen; i++)
+      {
+        DatoCadena.concat((char)buf[i]);
+      }
+      dato2 = DatoCadena.toFloat();
+      Serial.print("Temp recibido: ");
+      Serial.println(dato2);
+      lcd.setCursor(0, 1);  // Se pone el cursor en la primera posición de la pantalla.
+      lcd.print("Temp: "); lcd.print(dato2); lcd.write(BLANK); lcd.write(CIRCLE); lcd.print("C");
+      lcd.setCursor(15, 1); lcd.write(TEMP);
+    }
+  }
+}
+
---
+++ Códigos de Arduino/RF433MHz_Transmitter/RF433MHz_Transmitter.ino
@@ -0,0 +1,37 @@
+//Librerias
+#include "DHT.h"
+#include <VirtualWire.h>
+
+#define DHTPIN 2
+#define DHTTYPE DHT11
+DHT dht(DHTPIN, DHTTYPE);
+
+
+void setup()
+{
+  //Se inicia el sensor
+  dht.begin(); 
+  // Se inicializa el RF
+  vw_setup(2000); // velocidad: Bits per segundo
+}
+
+void loop()
+{
+  float h = dht.readHumidity(); //Se lee la humedad
+  float t = dht.readTemperature(); //Se lee la temperatura
+
+  char buf[VW_MAX_MESSAGE_LEN]; // Cadena para enviar
+  String str = "";
+
+  str = "i" + String(h); //convertimos el float a String y agregamos un inicio de trama
+  str.toCharArray(buf, sizeof(buf)); //convertimos el String en un array
+  vw_send((uint8_t *)buf, strlen(buf)); //Enviamos el array
+  vw_wait_tx(); //Esperamos hasta que el mensaje se envie
+
+  str = "f" + String(t); //convertimos el float a String y agramos un inicio de trama
+  str.toCharArray(buf, sizeof(buf)); //convertimos el String en un array
+  vw_send((uint8_t *)buf, strlen(buf)); ////Enviamos el array
+  vw_wait_tx(); //Esperamos hasta que el mensaje se envie
+
+  delay(20);
+}
